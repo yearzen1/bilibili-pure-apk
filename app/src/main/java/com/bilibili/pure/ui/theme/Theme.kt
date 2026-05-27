bbc2142d9669454ca1c1f3714444bdd0044c82e5
jiahui <1017714519@qq.com>
2026-05-27 18:50:00 +0800

    feat: dark mode; feat: 暗色模式

--- app/src/main/java/com/bilibili/pure/ui/theme/Theme.kt
+++ app/src/main/java/com/bilibili/pure/ui/theme/Theme.kt
@@ -1,9 +1,24 @@
 package com.bilibili.pure.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
+private val LightColorScheme = lightColorScheme()
+
+private val DarkColorScheme = darkColorScheme()
+
 @Composable
-fun BilibiliPureTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
+fun BilibiliPureTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
 }
